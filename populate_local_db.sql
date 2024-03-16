ddbb645c4eba88f2480f0ae1f10fbf5ffee53a76
Bartmax112 <bartmax112@gmail.com>
2024-03-16 14:12:15 +0100

    Added script to fill db with dummy data

---
+++ populate_local_db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE example_table(
+    id INTEGER PRIMARY KEY,
+    name varchar(40)
+);
+
+insert into example_table  VALUES (1, 'hello world 1');
+insert into example_table  VALUES (2, 'hello world 2');
+insert into example_table  VALUES (3, 'hello world 3');
+insert into example_table  VALUES (4, 'hello world 4');
+insert into example_table  VALUES (5, 'hello world 5');
+
